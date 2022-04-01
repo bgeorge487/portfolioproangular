8f093d069a8bf073d445605bad45d39acad1929f
Brian G <brigeo987@gmail.com>
2022-03-31 21:48:47 -0400

    imported modules to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { UserModule } from './user/user.module';
+import { CommodityModule } from './commodity/commodity.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule,
+    CommodityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
