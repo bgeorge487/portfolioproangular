3340d9dbed3722e81e1dee002eec8d7afd9b3a85
Geoffrey <geoffreyorama@gmail.com>
2022-04-10 18:03:18 -0400

    finally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'menu',
+    component: MenuComponent
+  },
+  {
+    path: 'register', 
+    component: RegisterComponent
+  },
+  {
+    path: 'login', 
+    component: LoginComponent
+  },
+  {
+    path: 'home', 
+    component: HomeComponent
+  },
+  { path: '**', component:NotFoundComponent  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
