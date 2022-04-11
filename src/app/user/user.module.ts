6d8fbbd1f7263eb1894d313f19c11c252cfef05d
Geoffrey <geoffreyorama@gmail.com>
2022-04-10 23:14:49 -0400

    updoot

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 
 
 @NgModule({
   declarations: [
-    UserProfileComponent
+    UserProfileComponent,
+    DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
 export class UserModule { }
